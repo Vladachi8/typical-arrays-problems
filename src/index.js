ec7c47f689886203d98520381f0bc72e3bbec187
Vladachi8 <Vladachi8@gmail.com>
2020-03-16 20:49:17 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }   else{
+        let min = array[0];
+        for (let i = 0; i < array.length; i++){
+            (min < array[i]) ? min = min : min = array[i];
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }   else{
+        let max = array[0];
+        for (let i = 0; i < array.length; i++){
+            (max > array[i]) ? max = max : max = array[i];
+        }
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+    if (array === undefined || array.length === 0){
+        return 0;
+    }   else{
+        return array.reduce(function(a,b) {return a+b;}) / array.length;
+    }
 }
